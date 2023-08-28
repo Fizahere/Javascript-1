4a7e8995347e698f72656f8c0855d3a31c7aad29
Fiza <131092372+Fizahere@users.noreply.github.com>
2023-08-28 11:09:12 +0500

    done

--- 8-first-api-project/app.js
+++ 8-first-api-project/app.js
@@ -1,28 +1,29 @@
 const buttonTwo = document.querySelector("#button2");
 const outputCustomer = document.querySelector("#customers");
 
-buttonTwo.addEventListener("click",function(event){
+buttonTwo.addEventListener("click", function (event) {
   event.preventDefault();
   fetch('customers.json')
-  .then(function(customersData){
-    return customersData.json();
-  })
-  .then(function(data){
-    // console.log(data,'data');
-    if(data.length > 0){
-      data.forEach(function(value){
-        // console.log(value,'value');
-        outputCustomer.append(`<tr>
-        <td>${value.id}</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        </tr>`)
-        
-      })
-    }
-  })
-  .catch(function(error){
-    console.log(error);
-  })
+    .then(function (customersData) {
+      return customersData.json();
+    })
+    .then(function (data) {
+      // console.log(data,'data');
+      if (data.length > 0) {
+        data.forEach(function (value) {
+          const tableRow = document.createElement("tr");
+          tableRow.innerHTML = ` <td>${value.id}</td>
+                                 <td>${value.name}</td>
+                                 <td>${value.company}</td>
+                                 <td>${value.phone}</td>
+                                 <td>${value.email}</td>`
+          // console.log(value,'value');
+          outputCustomer.append(tableRow);
+
+        })
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 })
